028f6f15b79ce36cef6f6c5350e203bba36f340c
afroze shaik <afrozeshaik24@gmail.com>
2025-06-07 06:58:11 +0530

    Added new code for [your feature/fix]

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,33 +2,26 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-   HomePage({super.key});
-  final user=FirebaseAuth.instance.currentUser;
+ HomePage({super.key});
 
-  void Signout() {
-    FirebaseAuth.instance.signOut();
+  final user = FirebaseAuth.instance.currentUser!;
+  //sign out user
+  void signUserOut() async {
+    await FirebaseAuth.instance.signOut();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [
-          IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: () {
-              // Add your logout logic here
-              Signout();
-            },
-          ),
-        ],
-        title: Text("Home Page"),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
+        title: const Text('Home Page'),
+        backgroundColor: const Color.fromARGB(255, 78, 148, 233),
+
+        actions: [IconButton(onPressed: signUserOut, icon: Icon(Icons.logout))],
       ),
-      body: Center(child: Text("LoGGED IN AS "+(user?.email ?? "Guest")+"!",
-      
+      body: Center(child: Text("Logged in as "+user.email!,
+      style: TextStyle(fontSize: 20, color: Colors.black54, fontWeight: FontWeight.bold
       )),
-    );
+    ));
   }
 }
